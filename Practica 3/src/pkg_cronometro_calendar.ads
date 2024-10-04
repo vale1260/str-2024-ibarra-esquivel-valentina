d30f3fd0417531b7508d1cd7a9da6498254aef4e
vale1260 <valentina.ibarra_e@mail.udp.cl>
2024-10-04 06:43:47 -0300

    .

--- Practica 3/src/pkg_cronometro_calendar.ads
+++
@@ -1,9 +0,0 @@
-with PKG_graficos;
-use PKG_graficos;
-
-with Ada.Calendar;
-use Ada.Calendar;
-
-package pkg_cronometro_calendar is
-   task actualizar1;
-end pkg_cronometro_calendar;
--- Practica 3/src/pkg_cronometro_realtime.ads
+++
@@ -1,9 +0,0 @@
-with PKG_graficos;
-use PKG_graficos;
-
-with Ada.Real_Time;
-use Ada.Real_Time;
-
-package pkg_cronometro_realtime is
-   task actualizar2;
-end pkg_cronometro_realtime;
