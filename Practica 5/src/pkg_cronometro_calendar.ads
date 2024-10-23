fc0299ba2e47f7544626eba6dd0ebb65f3396ab9
vale1260 <valentina.ibarra_e@mail.udp.cl>
2024-10-23 09:24:48 -0300

    .

--- Practica 5/src/pkg_cronometro_calendar.ads
+++
@@ -1,9 +0,0 @@
-with Ada.Text_IO; use Ada.Text_IO;
-with PKG_graficos; use PKG_graficos;
-with Ada.Calendar; use Ada.Calendar;
-
-package pkg_cronometro_calendar is
-   task type T_Cronometro_Calendar;
-   type Ptr_Cronometro_Calendar is access T_Cronometro_Calendar;
-   procedure Iniciar_Cronometro_Calendar;
-end pkg_cronometro_calendar;
